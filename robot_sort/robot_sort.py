700238040c2f0ad09aa84344af1951be157b6950
Chris Boydstun <chrisboydstun@gmail.com>
2020-04-17 11:08:04 -0500

    robot_sort done

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,7 +97,32 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
+        self.set_light_on()
+        self.swap_item()
+        while self.can_move_right():
+
+            if self.compare_item() == None:
+                # self.swap_item()
+                self.move_right()
+                self.swap_item()
+                if self.light_is_on():
+                    self.set_light_off()
+                    while self.can_move_left():
+                        self.move_left()
+
+            while self.light_is_on() == False and self.can_move_right:
+                if self.compare_item() == None:
+                    self.set_light_on()
+                    self.swap_item()
+                    # print(self.light_is_on(), self.compare_item())
+                elif self.compare_item() == -1:
+                    self.swap_item()
+                    self.move_right()
+                elif self.compare_item() == 1 or self.compare_item() == 0:
+                    self.move_right()
+                    # print('hi')
+
+            
 
 
 if __name__ == "__main__":
