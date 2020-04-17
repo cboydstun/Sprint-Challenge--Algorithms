a59d5e1a73beb98adf7d07b77652dd87c876bec7
Chris Boydstun <chrisboydstun@gmail.com>
2020-04-17 11:53:42 -0500

    recursive_count done

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -7,4 +7,15 @@ def count_th(word):
     
     # TBC
     
-    pass
+    count = 0
+    if len(word) < 2:
+        return 0
+    if word[0] + word[1] == "th" and len(word)  == 2:     
+        return 1
+    if word[0] + word[1] == "th": 
+        count += 1 + count_th(word[2:])
+    else: 
+        count += count_th(word[1:]) 
+    return count 
+
+print(count_th("thththth"))
